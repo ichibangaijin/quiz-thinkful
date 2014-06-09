0d16f481d749b04b63f1449faac0ce5362a7d610
Matt Highland <ichibangaijin77@gmail.com>
2014-06-09 15:19:39 -0700

    javascript now working

--- js/main.js
+++ js/main.js
@@ -1,19 +1,52 @@
-
-var firstQuestion = {
-    answerOne : ["Black Bass", "Large Mouth Bass", "Perch", "Small Mouth", "Striped Bass"]
-       
+var fishGame = {
+    questions: ["What type of fish is this?"],
+    answers: ["Black Bass", "Spotted Bass", "Small Mouth Bass", "Striped Bass"],
+    correctAnswer: "Striped Bass",
+    response : {
+                positive : "You are correct!",
+                negative : "Sorry, guess again!"
+    }
 };
 
+var q = 0;
+
+
+function question(q) {
+    var questions = fishGame.questions[q];
+    var answer = fishGame.correctAnswer;
+  var guess; 
+    $(".container").show();
+    $(".whatAmI").text(questions);
+    for (var qAnswer = 0; qAnswer < fishGame.answers.length; qAnswer++) {
+        $("ul").append("<li class='selector'>" + fishGame.answers[qAnswer] + "</li>");
+         }
+
+  console.log(answer);
+        $(".selector").click(function () {
+            guess = $(this).text();
+          if (guess == answer) {
+            alert(fishGame.response.positive);
+              q += 1;
+              return nextQuestion();
+              } else {
+              alert(fishGame.response.negative);
+                question(q);
+            }
+      });
+    }
+       
 
 
 
-$(document).ready(function(){
-  $("a").click(function(){
-    $("a, h3.letsPlay").hide();
-    $(".container").fadeToggle(500); 
-  });
-  for (var i = 0 ; i < firstQuestion.answerOne.length ; i++) 
-  $(".questionOne ul").append("<li>" + firstQuestion.answerOne[i] + "</li>"); 
-  
-});
+        function nextQuestion(q) {
+            question(q);
+        }
 
+        $(document).ready(function () {
+            $("a.letsPlay").click(function () {
+                $(this).hide();
+                question(q);
+            });
+
+
+        });
