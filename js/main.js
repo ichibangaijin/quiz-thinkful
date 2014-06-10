c9b10f10d8e7633fe91b447e24935e1c46a6a978
Matt Highland <ichibangaijin77@gmail.com>
2014-06-09 21:54:55 -0700

    rough working draft completed

--- js/main.js
+++ js/main.js
@@ -1,52 +1,55 @@
 var fishGame = {
-    questions: ["What type of fish is this?"],
-    answers: ["Black Bass", "Spotted Bass", "Small Mouth Bass", "Striped Bass"],
-    correctAnswer: "Striped Bass",
-    response : {
-                positive : "You are correct!",
-                negative : "Sorry, guess again!"
-    }
+  questions: ["What type of fish is this?", "What size is a keeper?", "What is a popular bait for this fish?", "When is the best time to fish?", "game over! Sorry I haven't finished yet."],
+  answers: [["Black Bass", "Spotted Bass", "Small Mouth Bass", "Striped Bass"], ["10in", "11in", "12in", "18in"], ["grass", "seaweed", "adult sturgeon", "shad"], ["nighttime", "daytime", "high tide", "changing tides"]],
+  correctAnswer: ["Striped Bass", "18in", "shad", "changing tides"],
+  response: {
+    positive: "You are correct!",
+    negative: "Sorry, guess again!"
+  }
 };
 
-var q = 0;
+var q;
 
 
 function question(q) {
-    var questions = fishGame.questions[q];
-    var answer = fishGame.correctAnswer;
-  var guess; 
-    $(".container").show();
-    $(".whatAmI").text(questions);
-    for (var qAnswer = 0; qAnswer < fishGame.answers.length; qAnswer++) {
-        $("ul").append("<li class='selector'>" + fishGame.answers[qAnswer] + "</li>");
-         }
+  var questions = fishGame.questions[q];
+  var answer = fishGame.correctAnswer[q];
+  var guess;
+  $(".whatAmI").show().text(questions);
+  for (var qAnswer = 0; qAnswer < fishGame.answers[q].length; qAnswer++) {
+    $("ul").append("<li class='selector'><input type='checkbox'>" + fishGame.answers[q][qAnswer] + "</input></li>");
+  }
 
   console.log(answer);
-        $(".selector").click(function () {
-            guess = $(this).text();
-          if (guess == answer) {
-            alert(fishGame.response.positive);
-              q += 1;
-              return nextQuestion();
-              } else {
-              alert(fishGame.response.negative);
-                question(q);
-            }
-      });
+  $(".selector").click(function () {
+    guess = $(this).text();
+    if (guess == answer) {
+      alert(fishGame.response.positive);
+      $("li").remove();
+      $(".whatAmI").hide();
+      q += 1;
+      nextQuestion(q);
+    } else {
+      alert(fishGame.response.negative);
+      $("ul li").remove();
+      question(q);
     }
-       
+  });
+}
 
 
+function nextQuestion(z) {
+  console.log(z);
+  question(z);
+}
 
-        function nextQuestion(q) {
-            question(q);
-        }
-
-        $(document).ready(function () {
-            $("a.letsPlay").click(function () {
-                $(this).hide();
-                question(q);
-            });
+$(document).ready(function () {
+  $("a.letsPlay").click(function () {
+    $(this).hide();
+    $(".container").show();
+    q = 0;
+    question(q);
+  });
 
 
-        });
+});
