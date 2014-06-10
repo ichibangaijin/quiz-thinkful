98c218d479438ae602a802ca9821847206d7bf84
Matt Highland <ichibangaijin77@gmail.com>
2014-06-10 09:41:03 -0700

    javascript now working and score implemented

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var fishGame = {
-  questions: ["What type of fish is this?", "What size is a keeper?", "What is a popular bait for this fish?", "When is the best time to fish?", "game over! Sorry I haven't finished yet."],
+  questions: ["What type of fish is this?", "What size is a keeper?", "What is a popular bait for this fish?", "When is the best time to fish?"],
   answers: [["Black Bass", "Spotted Bass", "Small Mouth Bass", "Striped Bass"], ["10in", "11in", "12in", "18in"], ["grass", "seaweed", "adult sturgeon", "shad"], ["nighttime", "daytime", "high tide", "changing tides"]],
   correctAnswer: ["Striped Bass", "18in", "shad", "changing tides"],
   response: {
@@ -8,34 +8,46 @@ var fishGame = {
   }
 };
 
-var q;
-
+var q = 0;
+var score = 0;
 
 function question(q) {
   var questions = fishGame.questions[q];
   var answer = fishGame.correctAnswer[q];
   var guess;
-  $(".whatAmI").show().text(questions);
+  $(".whatAmI p").show().text(questions);
   for (var qAnswer = 0; qAnswer < fishGame.answers[q].length; qAnswer++) {
     $("ul").append("<li class='selector'><input type='checkbox'>" + fishGame.answers[q][qAnswer] + "</input></li>");
+    $(".selector").click(function () {
+      score +=1;
+      if (q === 3) {
+        var gameover = score; 
+        $("#scorebox p").text(gameover + " tries to answer 4 questions");
+        gameOver();  
+      } else {
+        guess = $(this).text();
+        if (guess == answer) {
+          $("#scorebox p").text(fishGame.response.positive);
+          $("li").remove();
+          $(".whatAmI p").hide();
+          q += 1;
+          nextQuestion(q);
+        } else {
+          $("#scorebox p").text(fishGame.response.negative);
+          $("ul li").remove();
+          question(q);
+        }
+      }
+    });
   }
-
-  console.log(answer);
-  $(".selector").click(function () {
-    guess = $(this).text();
-    if (guess == answer) {
-      alert(fishGame.response.positive);
-      $("li").remove();
-      $(".whatAmI").hide();
-      q += 1;
-      nextQuestion(q);
-    } else {
-      alert(fishGame.response.negative);
-      $("ul li").remove();
-      question(q);
-    }
-  });
 }
+
+
+function gameOver(){
+  $("li").remove(); 
+  $(".whatAmI p").text("Thanks for Playing!");
+}
+
 
 
 function nextQuestion(z) {
@@ -47,6 +59,7 @@ $(document).ready(function () {
   $("a.letsPlay").click(function () {
     $(this).hide();
     $(".container").show();
+    $("#scorebox p").text("click on below answer");
     q = 0;
     question(q);
   });
