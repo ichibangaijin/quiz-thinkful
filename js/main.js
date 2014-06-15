ae389d501a0e8b3eb92b5f7860dc5fb5990cc201
Matt Highland <ichibangaijin77@gmail.com>
2014-06-14 17:47:57 -0700

    still trying to get score to function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var fishGame = {
   questions: ["What type of fish is this?", "What size is a keeper?", "What is a popular bait for this fish?", "When is the best time to fish?"],
   answers: [["Black Bass", "Spotted Bass", "Small Mouth Bass", "Striped Bass"], ["10in", "11in", "12in", "18in"], ["grass", "seaweed", "adult sturgeon", "shad"], ["nighttime", "daytime", "high tide", "changing tides"]],
-  correctAnswer: ["Striped Bass", "18in", "shad", "changing tides"],
+  correctAnswer: ["Striped Bass", "18in", "shad", "high tide"],
   response: {
     positive: "You are correct!",
     negative: "Sorry, guess again!"
@@ -12,21 +12,23 @@ var q = 0;
 var score = 0;
 
 function question(q) {
+  score += 1;
+
   var questions = fishGame.questions[q];
   var answer = fishGame.correctAnswer[q];
   var guess;
   $(".whatAmI p").show().text(questions);
   for (var qAnswer = 0; qAnswer < fishGame.answers[q].length; qAnswer++) {
     $("ul").append("<li class='selector'><input type='checkbox'>" + fishGame.answers[q][qAnswer] + "</input></li>");
+  }
     $(".selector").click(function () {
-      score +=1;
-      if (q === 3) {
+       if (q == 3) {
         var gameover = score; 
         $("#scorebox p").text(gameover + " tries to answer 4 questions");
         gameOver();  
       } else {
         guess = $(this).text();
-        if (guess == answer) {
+        if (guess === answer) {
           $("#scorebox p").text(fishGame.response.positive);
           $("li").remove();
           $(".whatAmI p").hide();
@@ -40,12 +42,14 @@ function question(q) {
       }
     });
   }
-}
+
+
 
 
 function gameOver(){
   $("li").remove(); 
   $(".whatAmI p").text("Thanks for Playing!");
+    console.log(answer);
 }
 
 
@@ -59,7 +63,7 @@ $(document).ready(function () {
   $("a.letsPlay").click(function () {
     $(this).hide();
     $(".container").show();
-    $("#scorebox p").text("click on below answer");
+    $("#scorebox p").text("click on your guess below");
     q = 0;
     question(q);
   });
